fd2077ef4aa0d32003ac309598e241e5e4c0a46a
Aryan Sheka <aryansheka1042@gmail.com>
2023-05-15 17:31:46 +0530

    Implement Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -48,15 +48,3 @@ class DataIngestion:
 
         except Exception as e:
             raise CustomException(e,sys)
-        
-
-if __name__ == "__main__":
-    data_ingestion= DataIngestion()
-    train_path,test_path= data_ingestion.initiate_data_ingestion()
-
-    data_transformation= DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_path,test_path)
-
-    model_trainer=ModelTrainer()
-    model_trainer.initiate_model_training(train_arr=train_arr,test_arr=test_arr)
-
